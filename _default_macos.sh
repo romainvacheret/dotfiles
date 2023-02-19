7fc6b95dca08254655ff7a639cc3635a18eea811
Romain Vacheret <romain.vacheret@gmail.com>
2023-02-19 18:10:54 +0100

    feat(setup): add macos settings

---
+++ _default_macos.sh
@@ -0,0 +1,8 @@
+# Show hidden files
+defaults write com.apple.finder appleshowallfiles true
+# Change default screen location
+defaults write com.apple.screencapture location /Users/romain/Pictures/screens
+# Allow key repeating
+defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
+# Note: delete '.ce' if it's not the community edition
+defaults write com.jetbrains.intellij.ce ApplePressAndHoldEnabled -bool false
