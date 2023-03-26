7e672c53ce510886057e22051aef8bf1813d095e
Romain Vacheret <romain.vacheret@gmail.com>
2023-03-26 14:06:25 +0200

    feat(setup): add macos defaults for anki

--- _default_macos.sh
+++ _default_macos.sh
@@ -6,3 +6,7 @@ defaults write com.apple.screencapture location /Users/romain/Pictures/screens
 defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
 # Note: delete '.ce' if it's not the community edition
 defaults write com.jetbrains.intellij.ce ApplePressAndHoldEnabled -bool false
+# Anki
+defaults write net.ankiweb.dtop NSAppSleepDisabled -bool true
+defaults write net.ichi2.anki NSAppSleepDisabled -bool true
+defaults write org.qt-project.Qt.QtWebEngineCore NSAppSleepDisabled -bool true
