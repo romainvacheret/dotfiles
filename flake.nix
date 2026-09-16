e9f92c8181dbb22cc8c280d96c5ea7e4b99b6131
Romain Vacheret <romain.vacheret@gmail.com>
2026-09-16 14:08:16 +0200

    feat(nix): add shared packages

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     home-manager = {
       url = "github:nix-community/home-manager/release-25.11";
@@ -10,15 +11,21 @@
     };
   };
 
-  outputs = { nixpkgs, home-manager, ... }:
+  outputs = { nixpkgs, nixpkgs-unstable, home-manager, ... }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
+      # Required to have nvim 0.12, is not in stable release
+      pkgsUnstable = nixpkgs-unstable.legacyPackages.${system};
     in
     {
       homeConfigurations."ubuntu" =
         home-manager.lib.homeManagerConfiguration {
           inherit pkgs;
+
+          extraSpecialArgs = {
+            inherit pkgsUnstable;
+          };
 
           modules = [
             ./hosts/ubuntu.nix
--- home/common.nix
+++ home/common.nix
@@ -1,9 +1,34 @@
-{ pkgs, ... }:
+{ pkgs, pkgsUnstable, ... }:
 
 {
   home.packages = [
+    pkgs.eza
+    pkgs.ghostty
+    pkgs.lazygit
+    pkgsUnstable.neovim
     pkgs.ripgrep
+    pkgs.superfile
+    pkgs.tokei
+    pkgs.tmux
+    pkgs.tree
   ];
+
+  home.file = {
+    ".config/ghostty/config" = {
+      source = ../.config/ghostty/config;
+      force = true;
+    };
+
+    ".config/nvim" = {
+      source = ../.config/nvim;
+      force = true;
+    };
+
+    ".tmux.conf" = {
+      source = ../.tmux.conf;
+      force = true;
+    };
+  };
 
   programs.home-manager.enable = true;
 }
